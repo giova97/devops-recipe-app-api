1e9bb3f8970f3b8ac8ea64a10d27867fd8445ae2
gio <g.aristizabal@altostratus.es>
2024-10-03 21:14:01 +0200

    update media and static volumes

---
+++ infra/setup/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "devops-gio"
+    key            = "tf-state-setup"
+    region         = "eu-west-1"
+    encrypt        = true
+    dynamodb_table = "devops-recipe-app-api-lock"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+
+  default_tags {
+    tags = {
+      Environment = "terraform.workspace"
+      Project     = var.project
+      Contact     = var.contact
+      ManagedBy   = "Terraform/setup"
+    }
+  }
+}
+
---
+++ infra/setup/variables.tf
@@ -0,0 +1,22 @@
+variable "tf_state_bucket" {
+  description = "Name of s3 bucket in AWS for storing Terraform state"
+  type        = string
+  default     = "devops-gio"
+}
+variable "tf_state_lock_table" {
+  description = "Name of dynamodb table in AWS for Terraform state locking"
+  type        = string
+  default     = "devops-recipe-app-api-lock"
+}
+
+variable "project" {
+  description = "Project name for tagging resources"
+  type        = string
+  default     = "recipe-app-api"
+}
+
+variable "contact" {
+  description = "Contact email"
+  type        = string
+  default     = "giorgos.georgiou.dev@gmail.com"
+}
