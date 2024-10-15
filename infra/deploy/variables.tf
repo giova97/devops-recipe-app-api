1f0fd0901640abb4372d69234ee4b90f3f8946a3
gio <g.aristizabal@altostratus.es>
2024-10-15 20:41:01 +0200

    added resources

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -1,17 +1,51 @@
 variable "prefix" {
   description = "Prefix for resources in AWS"
-  type        = string
   default     = "raa"
 }
 
 variable "project" {
   description = "Project name for tagging resources"
-  type        = string
   default     = "recipe-app-api"
 }
 
 variable "contact" {
   description = "Contact email for tagging resources"
-  type        = string
-  default     = "giorgos.georgiou.dev@gmail.com"
+  default     = "mark@example.com"
+}
+
+variable "db_username" {
+  description = "Username for the recipe app api database"
+  default     = "recipeapp"
+}
+
+variable "db_password" {
+  description = "Password for the Terraform database"
+}
+
+variable "ecr_proxy_image" {
+  description = "Path to the ECR repo with the proxy image"
+}
+
+variable "ecr_app_image" {
+  description = "Path to the ECR repo with the API image"
+}
+
+variable "django_secret_key" {
+  description = "Secret key for Django"
+}
+
+variable "dns_zone_name" {
+  description = "Domain name"
+  default     = "londonappdev.net"
+}
+
+variable "subdomain" {
+  description = "Subdomain for each environment"
+  type        = map(string)
+
+  default = {
+    prod    = "api"
+    staging = "api.staging"
+    dev     = "api.dev"
+  }
 }
